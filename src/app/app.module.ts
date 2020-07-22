d66d0ebe105defdeea263d96f745bc2fe10b676e
witchayaporn <witchayaporn@users.noreply.github.com>
2020-07-22 09:41:24 +0700

    update home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,13 +17,15 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
+import {MatCardModule} from '@angular/material/card';
 
 const materialComponent = [
   MatInputModule,
   MatButtonModule,
   MatGridListModule,
   MatToolbarModule,
-  MatIconModule
+  MatIconModule,
+  MatCardModule
 ];
 
 @NgModule({
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -11,7 +11,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [],
   imports: [
     CommonModule,
     RouterModule.forChild(routes)
